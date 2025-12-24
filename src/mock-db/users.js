ae0976db1874a8d56e80108dff56e9be98716a51
chairatnn <61372139+chairatnn@users.noreply.github.com>
2025-12-24 15:22:11 +0700

    refactor user router with api version control

---
+++ src/mock-db/users.js
@@ -0,0 +1,6 @@
+export const users = [
+  {id: "1", name: "Alice", email: "alice@example.com" },
+  {id: "2", name: "Bob", email: "bobby@example.com" },
+  {id: "3", name: "Charlie", email: "charlie@example.com" },
+  {id: "4", name: "Chairat", email: "chairat@example.com" },
+];
